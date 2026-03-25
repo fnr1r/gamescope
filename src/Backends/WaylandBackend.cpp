3944e5a7524f12208b673c1a818b3f2ce48b3712
fnrir <fnr1r0@protonmail.com>
2026-03-25 17:26:01 +0100

    feat(split-wl): move out WaylandFb impl

--- src/Backends/WaylandBackend.cpp
+++ src/Backends/WaylandBackend.cpp
@@ -95,58 +95,6 @@ namespace gamescope
     // CWaylandFb
     //////////////////
 
-    CWaylandFb::CWaylandFb( CWaylandBackend *pBackend, wl_buffer *pHostBuffer )
-        : CBaseBackendFb()
-        , m_pBackend     { pBackend }
-        , m_pHostBuffer  { pHostBuffer }
-    {
-        wl_buffer_add_listener( pHostBuffer, &s_BufferListener, this );
-    }
-
-    CWaylandFb::~CWaylandFb()
-    {
-        // I own the pHostBuffer.
-        wl_buffer_destroy( m_pHostBuffer );
-        m_pHostBuffer = nullptr;
-    }
-
-    void CWaylandFb::OnCompositorAcquire()
-    {
-        // If the compositor has acquired us, track that
-        // and increment the ref count.
-        if ( !m_bCompositorAcquired )
-        {
-            m_bCompositorAcquired = true;
-            IncRef();
-        }
-    }
-
-    void CWaylandFb::OnCompositorRelease()
-    {
-        // Compositor has released us, decrement rc.
-        //assert( m_bCompositorAcquired );
-
-        if ( m_bCompositorAcquired )
-        {
-            m_bCompositorAcquired = false;
-            DecRef();
-        }
-        else
-        {
-            xdg_log.errorf( "Compositor released us but we were not acquired. Oh no." );
-        }
-    }
-
-    void CWaylandFb::Wayland_Buffer_Release( wl_buffer *pBuffer )
-    {
-        assert( m_pHostBuffer );
-        assert( m_pHostBuffer == pBuffer );
-
-        xdg_log.debugf( "buffer_release: %p", pBuffer );
-
-        OnCompositorRelease();
-    }
-
     //////////////////
     // CWaylandConnector
     //////////////////
--- src/Backends/wayland/WaylandFb.cpp
+++ src/Backends/wayland/WaylandFb.cpp
@@ -1,6 +1,7 @@
 #include "WaylandFb.hpp"
 
 #include "callback_macro.hpp"
+#include "xdg_log.hpp"
 
 namespace gamescope {
 
@@ -8,4 +9,45 @@ const wl_buffer_listener CWaylandFb::s_BufferListener = {
 	.release = WAYLAND_USERDATA_TO_THIS(CWaylandFb, Wayland_Buffer_Release),
 };
 
+CWaylandFb::CWaylandFb(CWaylandBackend* pBackend, wl_buffer* pHostBuffer)
+	: CBaseBackendFb(), m_pBackend{pBackend}, m_pHostBuffer{pHostBuffer} {
+	wl_buffer_add_listener(pHostBuffer, &s_BufferListener, this);
 }
+
+CWaylandFb::~CWaylandFb() {
+	// I own the pHostBuffer.
+	wl_buffer_destroy(m_pHostBuffer);
+	m_pHostBuffer = nullptr;
+}
+
+void CWaylandFb::OnCompositorAcquire() {
+	// If the compositor has acquired us, track that
+	// and increment the ref count.
+	if (!m_bCompositorAcquired) {
+		m_bCompositorAcquired = true;
+		IncRef();
+	}
+}
+
+void CWaylandFb::OnCompositorRelease() {
+	// Compositor has released us, decrement rc.
+	// assert( m_bCompositorAcquired );
+
+	if (m_bCompositorAcquired) {
+		m_bCompositorAcquired = false;
+		DecRef();
+	} else {
+		xdg_log.errorf("Compositor released us but we were not acquired. Oh no.");
+	}
+}
+
+void CWaylandFb::Wayland_Buffer_Release(wl_buffer* pBuffer) {
+	assert(m_pHostBuffer);
+	assert(m_pHostBuffer == pBuffer);
+
+	xdg_log.debugf("buffer_release: %p", pBuffer);
+
+	OnCompositorRelease();
+}
+
+} // namespace gamescope
